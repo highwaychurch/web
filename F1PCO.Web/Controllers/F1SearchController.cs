1e16767bcce1ee35bf43b8d25976c682e812d00a
michaelnoonan <michaelnoonan@gmail.com>
2012-05-29 14:52:57 +1000

    F1SearchController is working with IoC/DI

---
+++ F1PCO.Web/Controllers/F1SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using F1PCO.Integration.F1;
+
+namespace F1PCO.Web.Controllers
+{
+    public class F1SearchController : ApiController
+    {
+        private readonly IF1PersonRepository _f1PersonRepository;
+
+        public F1SearchController(IF1PersonRepository f1PersonRepository)
+        {
+            _f1PersonRepository = f1PersonRepository;
+        }
+
+        public IEnumerable<F1Person> GetPeople()
+        {
+            return _f1PersonRepository.SearchByName("michael");
+        }
+    }
+}
